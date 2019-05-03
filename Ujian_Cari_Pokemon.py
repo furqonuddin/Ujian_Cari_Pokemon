c8cc09de371d77363d30cae9637d71804089dd51
furqonuddin <m.furqonuddin@gmail.com>
2019-05-03 14:47:28 +0700

    Ujian_Cari_Pokemon

---
+++ Ujian_Cari_Pokemon.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, jsonify, make_response, request
+import json
+import requests
+
+
+app = Flask(__name__)
+
+# =============================================================
+@app.route('/')
+def welcome():
+    return render_template('home.html')
+
+@app.route('/hasil', methods = ['POST', 'GET'])
+def hasil():
+    pokemon = request.form['namapokemon']
+    url = 'https://pokeapi.co/api/v2/pokemon/'
+    
+
+    try:
+        data = requests.get(url + pokemon.lower())
+        nama = data.json()['name']
+        gambar = data.json()['sprites']['front_default']
+        no_id = data.json()['id']
+        tinggi = data.json()['height']
+        berat = data.json()['weight']
+
+        profil = {
+            'nama' : nama,
+            'gambar' : gambar,
+            'id' : no_id,
+            'tinggi' : tinggi,
+            'berat' : berat
+        }
+
+        return render_template(
+            'hasil.html',
+            profil = profil
+        )
+
+    except:
+        return render_template('NotFound.html')        
+
+
+# not found display
+@app.errorhandler(404)
+def tidakfound(error):                                                  # deklarasikan parameter
+    return make_response('<h1>Maaf file tidak ditemukan</h1>')
+    # return make_response(
+    #     render_template('error.html')
+    # )
+
+if __name__ == '__main__':
+    app.run(debug = True) 
